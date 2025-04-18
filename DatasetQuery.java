c0768fdb70210c9e2a6d56f3ac4480055be54888
Chuying Huo <chuo8548@gmail.com>
2025-04-18 18:31:25 -0400

    adding file

--- DatasetQuery.java
+++ DatasetQuery.java
@@ -1,4 +1,9 @@
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface DatasetQuery<T> {
-
+    List<T> exactMatchQuery(String attribute, Object value);
+    List<T> rangeQuery(String attribute, Comparable lowerBound, Comparable upperBound);
+    List<T> rangeQuery(String attribute, Comparable upperBound);
+    List<T> averageQuery(String attribute, LocalDateTime startTime, LocalDateTime endTime);
 }
